ecd7e146c3fa67336333dd963a5f5833bef7db06
alejito <alejoecheverry33@gmail.com>
2025-04-17 06:04:20 -0500

    restaurando proyecto

---
+++ invitados/urls.py
@@ -0,0 +1,21 @@
+# paso 1  ---> para enlazar mi archivo urls de mi app al archivo urls de mi proyecto;
+#  Importo el path de mi archivo urls de mi proyecto a este archivo e importo las vistas de mi nueva app.
+
+
+from django.urls import path, include 
+from  invitados import views  
+ 
+
+urlpatterns = [
+
+path('', views.home, name="Home"),
+# path('menu_principal/', views.menu_principal, name="MenuPrincipal"),
+# path('base/', views.base, name="Base"),
+
+# path('', include('usuarios.urls')),
+
+
+
+
+]
+
